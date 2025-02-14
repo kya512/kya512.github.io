a3d46f58fb1fabe7f2cb7eb899df98b3c1e669ff
kya512 <mailto:your.email@example.com>
2025-02-14 03:29:02 +0100

    saint-valentin-project

---
+++ St_valentin/Cadeau.js
@@ -0,0 +1,11 @@
+const questionContainer = document.querySelector(".question-container");
+const noBtn = document.querySelector(".js-no-btn");
+
+// /change the postion of no button
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
